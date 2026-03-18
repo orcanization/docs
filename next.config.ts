f82d02a13cbc084a71f27e46a8bca878f4393aa9
marian <elias-marian@web.de>
2026-03-18 22:11:39 +0100

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,6 @@
+import nextra from "nextra"
+
+const withNextra = nextra({
+})
+
+module.exports = withNextra()
---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import { Footer, Layout, Navbar } from 'nextra-theme-docs'
+import { Banner, Head } from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
+import 'nextra-theme-docs/style.css'
+
+export const metadata = {
+    // Define your metadata here
+    // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+}
+
+const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
+const navbar = (
+    <Navbar
+        logo={<b>Orca</b>}
+    />
+)
+const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
+
+export default async function RootLayout({ children }) {
+    return (
+        <html lang="en" dir="ltr"
+            // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+            suppressHydrationWarning
+        >
+        <Head>
+        </Head>
+        <body>
+        <Layout
+            banner={banner}
+            navbar={navbar}
+            pageMap={await getPageMap()}
+            docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+            footer={footer}
+        >
+            {children}
+        </Layout>
+        </body>
+        </html>
+    )
+}
